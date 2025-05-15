6859ea617a57ef6b8a7be5446c90038a5fbd3906
1256ID <112385041+1256ID@users.noreply.github.com>
2025-05-15 11:23:54 +0200

    Adding Models and Services.

---
+++ EmployeesApp.Web/Services/EmployeeService.cs
@@ -0,0 +1,13 @@
+﻿using EmployeesApp.Web.Models;
+
+namespace EmployeesApp.Web.Services
+{
+    public class EmployeeService
+    {
+        private List<Employee> employees = [];
+
+        public void Add(Employee employee) => employees.Add(employee);
+        public Employee[] GetAll() => employees.ToArray();
+        public Employee? GetById(int id) => employees.SingleOrDefault(c => c.Id.Equals(id));
+    }
+}
